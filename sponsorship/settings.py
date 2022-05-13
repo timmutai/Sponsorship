d0fd97de6a3b55771ea44cc22c7ab58ef0bd3f22
timmutai <timomutai@gmail.com>
2022-05-13 23:55:50 +0300

    add

--- sponsorship/settings.py
+++ sponsorship/settings.py
@@ -143,7 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='tmtrade21@gmail.com'
-EMAIL_HOST_PASSWORD='Osteen@1741'
+EMAIL_HOST_PASSWORD='Osteen@10741'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 
